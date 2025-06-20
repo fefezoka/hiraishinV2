cdf76cac64a14f2c97864cc804b552beba32c064
Felipe Brito <89111098+fefezoka@users.noreply.github.com>
2025-06-20 01:13:35 -0300

    wtf dotnetttttt

--- backend/Hiraishin.API/Program.cs
+++ backend/Hiraishin.API/Program.cs
@@ -29,33 +29,7 @@ builder.Services.AddHttpClient<IHiraishinService, HiraishinService>(client =>
 
 builder.Services.AddSwaggerGen(swagger =>
 {
-    swagger.SwaggerDoc("v1", new OpenApiInfo { Title = "Sebo", Version = "v1" });
-    swagger.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-    {
-        Description =
-                "JWT Authorization Header\r\n\r\n" +
-                "Informe seu token segundo o formato: 'Bearer [Token]'.\r\n\r\n" +
-                "Exemplo: 'Bearer ABC123DFG456'",
-        Name = "Authorization",
-        In = ParameterLocation.Header,
-        Type = SecuritySchemeType.ApiKey,
-        Scheme = "Bearer",
-        BearerFormat = "JWT",
-    });
-    swagger.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        {
-            new OpenApiSecurityScheme
-            {
-                Reference = new OpenApiReference
-                {
-                    Type = ReferenceType.SecurityScheme,
-                    Id = "Bearer"
-                }
-            },
-            Array.Empty<string>()
-        }
-     });
+    swagger.SwaggerDoc("v1", new OpenApiInfo { Title = "Hiraishin", Version = "v1" });
 });
 
 builder.Services.AddCors(options =>
