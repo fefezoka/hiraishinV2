dd97d6824ffacff81b8fae3365e389180a53073a
Felipe Brito <89111098+fefezoka@users.noreply.github.com>
2025-06-21 00:29:55 -0300

    change baseurl

--- frontend/src/service/axios.ts
+++ frontend/src/service/axios.ts
@@ -1,9 +1,12 @@
 import { default as instance } from 'axios';
 
+const isDev = process.env.NODE_ENV === 'development';
+const baseURL = isDev ? 'http://localhost:3001/api' : 'http://141.148.138.17/api';
+
 const axios = instance.create();
 
 axios.interceptors.request.use((config) => {
-  config.baseURL = 'http://localhost:3001/api';
+  config.baseURL = baseURL;
   config.headers['X-RIOT-TOKEN'] = process.env.RIOT_API_KEY;
   return config;
 });
