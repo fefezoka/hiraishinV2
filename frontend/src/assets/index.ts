f53fd413637515e2f29c884b12304e3a81a14ee6
Felipe Brito <89111098+fefezoka@users.noreply.github.com>
2025-06-25 20:24:31 -0300

    adicionando mais um gif de loading

--- frontend/src/assets/index.ts
+++ frontend/src/assets/index.ts
@@ -2,5 +2,6 @@ import mpengu from './mpengu.gif';
 import hiraishin from './hiraishin.png';
 import david from './david.jpg';
 import spinner from './spinner.svg';
+import penguDab from './pengu-dab.gif';
 
-export { mpengu, hiraishin, david, spinner };
+export { mpengu, hiraishin, david, spinner, penguDab };
--- frontend/src/components/loading.tsx
+++ frontend/src/components/loading.tsx
@@ -1,12 +1,16 @@
-import { mpengu } from '@/assets';
-import Image from 'next/image';
-import React from 'react';
+import { mpengu, penguDab } from '@/assets';
+import Image, { StaticImageData } from 'next/image';
+import React, { useState } from 'react';
 
 export const Loading = () => {
+  const [src] = useState<StaticImageData>(
+    [mpengu, penguDab][Math.floor(Math.random() * 2)]
+  );
+
   return (
     <div className="flex items-center justify-center flex-col">
       <div className="w-[360px] h-[360px] relative">
-        <Image src={mpengu} alt="" fill />
+        <Image src={src} alt="" fill />
       </div>
       <span className="text-2xl">Carregando...</span>
     </div>
